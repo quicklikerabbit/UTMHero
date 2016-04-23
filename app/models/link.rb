dd2cea141b5778f77813c60ef63fe930b151fb08
Steve Richards <steven.d.richards@gmail.com>
2016-04-23 01:23:59 +0000

    added database and form validations

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,3 +1,11 @@
 class Link < ActiveRecord::Base
+
+  validates :website_url, presence: true
+  validates :campaign_source, presence: true
+  validates :campaign_medium, presence: true
+  validates :campaign_name, presence: true
+  validates :campaign_source, presence: true
+
+
   belongs_to :client
 end
