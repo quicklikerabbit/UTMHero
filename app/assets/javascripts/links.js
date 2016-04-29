ff2d7b7682f2b58b2ece64efee9edaf1752fca4a
Steve Richards <steven.d.richards@gmail.com>
2016-04-29 23:50:38 +0000

    working button, scrolls to top of form when clicked

--- app/assets/javascripts/links.js
+++ app/assets/javascripts/links.js
@@ -1,4 +1,9 @@
 $(function() {
+  $("#get-started-button").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#link-form").offset().top
+    }, 1000);
+  });
   $( document ).ajaxComplete(function() {
     $('#create-link-overlay').removeClass('hidden');
   });
