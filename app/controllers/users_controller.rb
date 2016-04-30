457ac7bd8c69ede48c1d12e28b5cdc7cd92c6c20
Steve Richards <steven.d.richards@gmail.com>
2016-04-30 02:12:47 +0000

    form page made, looks terrible

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,11 +6,19 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
   end
 
   def edit
+  end
+
+  protected
+
+  def user_params
+    params.require(:user).permit(:email, :first_name, :last_name, :password, :password_confirmation)
   end
 end
