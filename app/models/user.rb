45b454e4feebdc26ea82ba93076eef5cea2c2614
Steve Richards <steven.d.richards@gmail.com>
2016-05-18 23:07:12 +0000

    email validations added to forms and model, styling changes to improve user experience

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,9 +8,18 @@ class User < ActiveRecord::Base
 
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email, presence: true
+  validates :email, presence: true, uniqueness: true
+  validate :valid_email
   validates :password,
   length: { in: 6..20 }, on: :create
 
+  private
 
+  VALID_EMAIL_REGEX = /@/
+
+  def valid_email
+    unless VALID_EMAIL_REGEX.match(self.email)
+      errors.add(:email, "must be valid")
+    end
+  end
 end
