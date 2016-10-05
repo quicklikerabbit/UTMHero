62ecaf2a1f17f2ee56e2f1c550952d638088d04d
vagrant <vagrant@lighthouse-labs>
2016-10-05 06:15:08 +0000

    grids working, table styling is better

--- app/assets/javascripts/links.js
+++ app/assets/javascripts/links.js
@@ -1,9 +1,4 @@
 $(function() {
-  $("#get-started-button").click(function() {
-    $('html, body').animate({
-        scrollTop: $("#link-form").offset().top
-    }, 1000);
-  });
   $( document ).ajaxComplete(function() {
     $('#create-link-overlay').removeClass('hidden').fadeTo(500, 0.9);
   });
@@ -24,11 +19,6 @@ $(function() {
 });
 
 $(function() {
-  // Setup - add a text input to each footer cell
-  $('#links_table thead tr#filterrow th').each( function () {
-      var title = $('#links_table thead th').eq( $(this).index() ).text();
-      $(this).html( '<input type="text" placeholder="Search '+title+'" />' );
-  } );
 
 // Apply the filter
   $("#links_table thead input").on( 'keyup change', function () {
@@ -37,28 +27,6 @@ $(function() {
           .search( this.value )
           .draw();
   } );
-  // DataTable
-  var table = $('#links_table').removeAttr('width').DataTable( {
-        orderCellsTop: true,
-        order: [[ 2, "desc" ]],
-        sDom: '<"top"fli>rt<"bottom"p><"clear">',
-        autoWidth: false,
-        columns: [
-          { width: "150px" },
-          { width: "150px" },
-          { width: "150px" },
-          { width: "150px" },
-          { width: "150px" },
-          { width: "150px" },
-          { width: "150px" },
-          { width: "150px" },
-          { width: "150px" },
-          { width: "150px" },
-          { width: "150px" },
-          { width: "150px" }
-        ],
-        fixedColumns : true
-    });
   $('.showHideColumn').on( 'click', function () {
     var tableColumn = table.column($(this).attr('data-columnindex'));
     tableColumn.visible(!tableColumn.visible());
