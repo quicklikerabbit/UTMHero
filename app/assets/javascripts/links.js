4df89b4f4f20f1908312674f7ba66bcb1ec682d3
vagrant <vagrant@lighthouse-labs>
2016-10-21 05:38:44 +0000

    started pagination, sortable columns, search in card panel

--- app/assets/javascripts/links.js
+++ app/assets/javascripts/links.js
@@ -11,11 +11,66 @@ $(function() {
           .search( this.value )
           .draw();
   } );
-  $('.showHideColumn').on( 'click', function () {
-    var tableColumn = table.column($(this).attr('data-columnindex'));
-    tableColumn.visible(!tableColumn.visible());
+});
+
+$(function(){
+  var $rows = $('#links_table_body tr');
+  $('#links_search').keyup(function() {
+    var val = $.trim($(this).val()).replace(/ +/g, ' ').toLowerCase();
+
+    $rows.show().filter(function() {
+        var text = $(this).text().replace(/\s+/g, ' ').toLowerCase();
+        return !~text.indexOf(val);
+    }).hide();
   });
-  $('.showHideColumn').on( 'click', function () {
-    $(this).toggleClass('selected');
+});
+
+function sortTable(table, col, reverse) {
+    var tb = table.tBodies[0], // use `<tbody>` to ignore `<thead>` and `<tfoot>` rows
+        tr = Array.prototype.slice.call(tb.rows, 0), // put rows into array
+        i;
+    reverse = -((+reverse) || -1);
+    tr = tr.sort(function (a, b) { // sort rows
+        return reverse // `-1 *` if want opposite order
+            * (a.cells[col].textContent.trim() // using `.textContent.trim()` for test
+                .localeCompare(b.cells[col].textContent.trim())
+               );
+    });
+    for(i = 0; i < tr.length; ++i) tb.appendChild(tr[i]); // append each row in order
+}
+
+function makeSortable(table) {
+    var th = table.tHead, i;
+    th && (th = th.rows[0]) && (th = th.cells);
+    if (th) i = th.length;
+    else return; // if no `<thead>` then do nothing
+    while (--i >= 0) (function (i) {
+        var dir = 1;
+        th[i].addEventListener('click', function () {sortTable(table, i, (dir = 1 - dir))});
+    }(i));
+}
+
+function makeAllSortable(parent) {
+    parent = parent || document.body;
+    var t = parent.getElementsByTagName('table'), i = t.length;
+    while (--i >= 0) makeSortable(t[i]);
+}
+
+window.onload = function () {makeAllSortable();};
+
+$(function() {
+  $('#links_table_body').pagination({
+      dataSource: [],
+      callback: function(data, pagination) {
+          // template method of yourself
+          var html = template(data);
+          dataContainer.html(html);
+      }
+  });
+
+  $.get('/links', function(data){
+    $.each(JSON.parse(data), function(index, element){
+      console.log(element);
+    });
   });
 });
